c5b90d87867b455df323a65e3ef4429e11b3210d
Estefanía García Estévez <fanigarcivez2003@gmail.com>
2022-10-13 04:04:28 -0500

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'module': true //para error: module' is not defined.eslint-no-undef
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
